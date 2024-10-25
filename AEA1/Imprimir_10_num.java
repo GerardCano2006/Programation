75f03bfd91a71b57b9538c7b0ea662667c9154a4
Gerard Cano <gecavi2006@gmail.com>
2024-10-25 17:29:41 +0200

    hello world

--- AEA1/Imprimir_10_num.java
+++ AEA1/Imprimir_10_num.java
@@ -2,10 +2,9 @@ package AEA1;
 
 public class Imprimir_10_num {
 	public static void main(String[] args){
-
 		for (int i=1; i < 11; i++) {
-			System.out.println(i);
-			
+			System.out.print(i);
+			System.out.println();
 		}
 	}
 }
