db286dbe0f3d50e2b5c9ac48bd686f838706fcc8
Gerard Cano <gecavi2006@gmail.com>
2024-10-25 17:31:36 +0200

    sout ln canvi

--- AEA1/Imprimir_10_num.java
+++ AEA1/Imprimir_10_num.java
@@ -3,8 +3,7 @@ package AEA1;
 public class Imprimir_10_num {
 	public static void main(String[] args){
 		for (int i=1; i < 11; i++) {
-			System.out.print(i);
-			System.out.println();
+			System.out.println(i);
 		}
 	}
 }
