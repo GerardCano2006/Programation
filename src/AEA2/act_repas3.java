59696fc2490a2572e0a15df94c01ab4a47b4c238
Gerard Cano <gecavi2006@gmail.com>
2024-12-27 20:36:34 +0100

    Commit 9

--- src/AEA2/act_repas3.java
+++ src/AEA2/act_repas3.java
@@ -8,8 +8,7 @@ public class act_repas3 {
         int sum1 = 0;
         int sum2 = 0;
 
-        //Matriu 1
-
+        // Matriu 1
         System.out.println("De quàntes files vols la 1a matriu? ");
         int files1 = lector.nextInt();
 
@@ -22,15 +21,13 @@ public class act_repas3 {
             for (int j = 0; j < matriu1[i].length; j++) {
                 System.out.println("Quin valor vols que hi hagi en la posició: " + i + " " + j);
                 int valor1 = lector.nextInt();
-                if (valor1 < 0 || valor1 > 10){
+                while (valor1 < 0 || valor1 > 10) {
                     System.out.println("ERROR, ha de ser un valor entre 0 i 10");
                     System.out.println("Quin valor vols que hi hagi en la posició: " + i + " " + j);
                     valor1 = lector.nextInt();
                 }
-                else {
-                    matriu1[i][j] = valor1;
-                    sum1 = sum1 + matriu1[i][j];
-                }
+                matriu1[i][j] = valor1;
+                sum1 = sum1 + matriu1[i][j];
             }
         }
 
@@ -41,33 +38,44 @@ public class act_repas3 {
             }
             System.out.println();
         }
-        System.out.println("");
         System.out.println("La suma total de la 1a matriu és: " + sum1);
 
 
-        //Matriu 2
 
+
+
+
+
+
+
+        // Matriu 2
         System.out.println("De quàntes files vols la 2a matriu? ");
         int files2 = lector.nextInt();
 
         System.out.println("De quàntes columnes vols la 2a matriu? ");
         int columnes2 = lector.nextInt();
 
-        int matriu2[][] = new int[files2][columnes2];
+        if (files1 != files2 || columnes1 != columnes2) {
+            System.out.println("ERROR: Les matrius han de tenir les mateixes dimensions per a ser sumades.");
+            System.out.println("De quàntes files vols la 2a matriu? " + "(" + files1 + ")");
+            files2 = lector.nextInt();
 
+            System.out.println("De quàntes columnes vols la 2a matriu? " + "(" + columnes1 + ")");
+            columnes2 = lector.nextInt();
+        }
+
+        int matriu2[][] = new int[files2][columnes2];
         for (int i = 0; i < matriu2.length; i++) {
             for (int j = 0; j < matriu2[i].length; j++) {
                 System.out.println("Quin valor vols que hi hagi en la posició: " + i + " " + j);
                 int valor2 = lector.nextInt();
-                if (valor2 < 0 || valor2 > 10){
+                if (valor2 < 0 || valor2 > 10) {
                     System.out.println("ERROR, ha de ser un valor entre 0 i 10");
                     System.out.println("Quin valor vols que hi hagi en la posició: " + i + " " + j);
                     valor2 = lector.nextInt();
                 }
-                else {
-                    matriu2[i][j] = valor2;
-                    sum2 = sum2 + matriu2[i][j];
-                }
+                matriu2[i][j] = valor2;
+                sum2 = sum2 + matriu2[i][j];
             }
         }
 
@@ -78,14 +86,31 @@ public class act_repas3 {
             }
             System.out.println();
         }
-        System.out.println("");
         System.out.println("La suma total de la 2a matriu és: " + sum2);
 
 
-        //Suma matrius
 
+
+
+
+
+
+        // Suma de matrius
+        int[][] sum_matrius = new int[files1][columnes1];
+        for (int i = 0; i < files1; i++) {
+            for (int j = 0; j < columnes1; j++) {
+                sum_matrius[i][j] = matriu1[i][j] + matriu2[i][j];
+            }
+        }
+
+        System.out.println("");
+        System.out.println("Matriu suma:");
+        for (int i = 0; i < sum_matrius.length; i++) {
+            for (int j = 0; j < sum_matrius[i].length; j++) {
+                System.out.print(sum_matrius[i][j] + " ");
+            }
+            System.out.println();
+        }
         System.out.println("La suma TOTAL de les dues matrius és: " + (sum1 + sum2));
-
-
     }
 }
