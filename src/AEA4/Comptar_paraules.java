ba642b20eb09c1826012f1eb46811befb2af1962
Gerard Cano <gecavi2006@gmail.com>
2025-04-25 17:35:27 +0200

    Commit AEA4

--- src/AEA4/Comptar_paraules.java
+++ src/AEA4/Comptar_paraules.java
@@ -13,20 +13,21 @@ public class Comptar_paraules {
             Scanner lector = new Scanner(f);
 
             //Fitxer sortida
-            PrintStream fitxer_sortida = new PrintStream("C:\\Users\\Usuario\\Desktop\\Programacion\\src\\AEA4\\sortida.txt");
+            PrintStream sortida = new PrintStream("C:\\Users\\Usuario\\Desktop\\Programacion\\src\\AEA4\\sortida.txt");
 
             int comptador = 0;
 
-            //Primer, copiem tot el contingut al fitxer de sortida i comptem paraules
+            //Comptem les paraules llegides
             while (lector.hasNext()) {
                 String paraula = lector.next();
                 comptador++;        //Comptem cada vegada que llegeix una paraula
-                fitxer_sortida.print(paraula + " "); //Copiem la paraula + espai
             }
 
             //Imprimir resultat
             System.out.println("Hem comptat " + comptador + " paraules");
             System.out.println("Resultat escrit a sortida.txt");
+
+            sortida.print("Hem comptat " + comptador + " paraules");        //Copiem el resultat a "sortida.txt"
 
         }
         catch (FileNotFoundException e) {       //En cas que no trobi el fitxer "entrada.txt"
